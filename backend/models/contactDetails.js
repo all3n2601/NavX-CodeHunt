dcdc92af62584863b2593212bf6ff4fc811e3be7
rahulpalanivel <palanivelrahul45@gmail.com>
2024-02-23 04:55:47 +0530

    commit controllers

--- backend/models/contactDetails.js
+++ backend/models/contactDetails.js
@@ -26,7 +26,4 @@ const contactSchema = new mongoose.Schema({
   },
 });
 
-// DB_URI=mongodb+srv://allen:allen123@navxcluster.xmtvylz.mongodb.net/?retryWrites=true&w=majority&appName=NavXCluster
-// PORT=4000
-
 module.exports = mongoose.model("Contact", contactSchema);
