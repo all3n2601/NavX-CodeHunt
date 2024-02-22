eea3d234a47e55ec643eb18f63357cab24398635
rahulpalanivel <palanivelrahul45@gmail.com>
2024-02-23 03:24:53 +0530

    commit contact.js

---
+++ frontend/src/pages/ContactUsPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
+import Contact from "../components/shared/contact";
+
+function SignInPage() {
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default SignInPage;
