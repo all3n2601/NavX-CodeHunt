b463b787f821562d706d161a9e236be176d7507e
rahulpalanivel <palanivelrahul45@gmail.com>
2024-02-23 07:09:12 +0530

    commit pages

--- frontend/src/components/core/carPool.js
+++ frontend/src/components/core/carPool.js
@@ -7,22 +7,63 @@ import "./map.css";
 import { useNavigate } from "react-router-dom";
 
 const CarPool = () => {
-
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate("/matchusers");
-      }
-    const [startDestination, setStartDestination] = useState("VITAP");
-    const [endDestination, setEndDestination] = useState("Vijaywada");
-    const places = [
-        "Chandralapuda", "Keasara", "Paritala", "Kondapalli", "Ibrahimpatnam", "Guntupalli", "Vijaywada",
-        "Kondapeta", "Vadamadu", "Dondapadu", "Thullur", "Abbarajupalem", "Rayapudi", "Tadepalle",
-        "Kothapalli", "Sc Colony", "Happy Nest", "Nelapal", "Velagapudi", "Moduga", "Vadeshwaram",
-        "Achampet", "Nekalu", "Shakamuru", "VITAP", "Amaravati", "Malkapuram", "Mangalagiri",
-        "Pedapalam", "Thandurayogesh", "Dywaswarui", "Inavolu", "Manadam", "Krishnayapelam", "Kantheru",
-        "Krosur", "Pedaparani", "TaikondaRd", "Nerukonda", "D Rd", "Kuragallu", "Pedakakani",
-        "Satenapalle", "Komerapudi", "Siripuram", "Peericherla", "Medikondara", "Pothur", "Guntur"
-      ];
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/matchusers");
+  };
+  const [startDestination, setStartDestination] = useState("VITAP");
+  const [endDestination, setEndDestination] = useState("Vijaywada");
+  const places = [
+    "Chandralapuda",
+    "Keasara",
+    "Paritala",
+    "Kondapalli",
+    "Ibrahimpatnam",
+    "Guntupalli",
+    "Vijaywada",
+    "Kondapeta",
+    "Vadamadu",
+    "Dondapadu",
+    "Thullur",
+    "Abbarajupalem",
+    "Rayapudi",
+    "Tadepalle",
+    "Kothapalli",
+    "Sc Colony",
+    "Happy Nest",
+    "Nelapal",
+    "Velagapudi",
+    "Moduga",
+    "Vadeshwaram",
+    "Achampet",
+    "Nekalu",
+    "Shakamuru",
+    "VITAP",
+    "Amaravati",
+    "Malkapuram",
+    "Mangalagiri",
+    "Pedapalam",
+    "Thandurayogesh",
+    "Dywaswarui",
+    "Inavolu",
+    "Manadam",
+    "Krishnayapelam",
+    "Kantheru",
+    "Krosur",
+    "Pedaparani",
+    "TaikondaRd",
+    "Nerukonda",
+    "D Rd",
+    "Kuragallu",
+    "Pedakakani",
+    "Satenapalle",
+    "Komerapudi",
+    "Siripuram",
+    "Peericherla",
+    "Medikondara",
+    "Pothur",
+    "Guntur",
+  ];
 
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -40,16 +81,20 @@ const CarPool = () => {
       zoom: zoom,
     });
 
-    new maptilersdk.Marker({color: "#FF0000"})
-      .setLngLat([80.49,16.49731])
+    new maptilersdk.Marker({ color: "#FF0000" })
+      .setLngLat([80.49, 16.49731])
       .addTo(map.current);
   }, [vitap.lng, vitap.lat, zoom]);
 
-
   return (
     <div className="flex h-screen border-2">
-      <div className="flex w-[30%] h-full flex-col items-center justify-center">
+      <div className="flex w-[30%] h-full flex-col items-center justify-center bg-white dark:bg-gray-900">
         <div className="h-1/2 flex flex-col justify-center items-center">
+          <div className="h-1/2">
+            <p className="text-amber-500 text-3xl px-6">
+              Choose Your Start and End Destination
+            </p>
+          </div>
           <div className="flex flex-col items-center mb-8">
             <label htmlFor="startDestination" className="sr-only">
               Start Destination
@@ -61,7 +106,9 @@ const CarPool = () => {
               className="block w-full px-20 py-2 text-purple-700 bg-white border rounded-lg focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             >
               {places.map((place, index) => (
-                <option key={index} value={place}>{place}</option>
+                <option key={index} value={place}>
+                  {place}
+                </option>
               ))}
             </select>
           </div>
@@ -70,33 +117,26 @@ const CarPool = () => {
               End Destination
             </label>
             <select
-             value={endDestination}
-             onChange={(e) => setEndDestination(e.target.value)}
+              value={endDestination}
+              onChange={(e) => setEndDestination(e.target.value)}
               id="startDestination"
               className="block w-full px-20 py-2 text-purple-700 bg-white border rounded-lg focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             >
               {places.map((place, index) => (
-                <option key={index} value={place}>{place}</option>
+                <option key={index} value={place}>
+                  {place}
+                </option>
               ))}
             </select>
           </div>
 
           <button
-          onClick={handleClick}
+            onClick={handleClick}
             type="button"
             class="text-white m-5 bg-amber-700 hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-amber-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
           >
             Book Now!
           </button>
-
-
-        </div>
-
-        <div className="h-1/2">
-          <p className="text-amber-500 text-7xl">
-            Choose
-            <br /> Your Start and End Destination
-          </p>
         </div>
       </div>
 
--- frontend/src/components/shared/Home.js
+++ frontend/src/components/shared/Home.js
@@ -5,17 +5,15 @@ import carlogo from "../../assets/car-logo-vector.png";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-
-
   const navigate = useNavigate();
 
   const handleClickBike = () => {
     navigate("/bikepool");
-  }
+  };
 
   const handleClickCar = () => {
     navigate("/carpool");
-  }
+  };
 
   return (
     <div className="relative flex flex-col justify-center items-center h-screen">
@@ -27,29 +25,35 @@ function Home() {
         muted
       />
 
-      <h1 className="text-white text-6xl font-bold absolute top-12 text-center">
+      <h1 className="text-white text-6xl font-bold absolute top-12 text-center text-amber-600">
         Form Your Pool Now
       </h1>
 
       <div className="absolute w-full h-full flex justify-between z-10">
         <div className="p-20 top-45">
-        <button onClick={handleClickBike} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+          <button
+            onClick={handleClickBike}
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"
+          >
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Book A Bike
+              Pool A Bike
             </span>
           </button>
-          <h3 className="text-white">
+          <h3 className="text-amber-600">
             Order a Bike Pool <br /> for 2 People Max
           </h3>
         </div>
 
         <div className="p-20 top-45">
-          <button onClick={handleClickCar} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+          <button
+            onClick={handleClickCar}
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"
+          >
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Book A Car
+              Pool A Car
             </span>
           </button>
-          <h3 className="text-white">
+          <h3 className="text-amber-600">
             Order a Car Pool <br /> for 5 People Max
           </h3>
         </div>
